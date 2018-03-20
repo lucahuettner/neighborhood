22191fdc0458eaf696de74c1b3ebdbaffbf8291c
Luca Huettner <luca.huettner@gmail.com>
2018-03-20 14:12:51 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+// jscs:disable validateQuoteMarks
+import React, { Component } from 'react';
+import Container from './Container.js';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Container/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Container.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Map from './Map.js';
+import { GoogleApiWrapper } from 'google-maps-react';
+
+class Container extends Component {
+  state = {
+    locations: [
+      {name: 'Festungsruine Hohentwiel', location: {lat: 47.7642964, lng: 8.818025}}
+    ]
+  };
+  render() {
+    fetch('https://api.foursquare.com/v2/venues/4c7d03d548c6236a972a6311?client_id=3KDKYVPQIJ2BMFAWJB5K2EE4LUEOHPYZYJQN55SYCGY50GNW&client_secret=F0GMCYEDRANIHBSG5501YDGIF1IKDJL4CZBRUHUMCRAKK55J&ll=40.7,-74&query=sushi&v=20180210')
+    .then((response) => response.json())
+    .then((obj) => console.log(obj.response));
+    return (
+      <div>
+        <h1>Neighborhood</h1>
+        <Map google={this.props.google} locations={this.state.locations} />
+      </div>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyARmShZ82k5hknINJ7jMndOnRDNrEas9Sk',
+})(Container);
