bb5f389ff6a233b1edc2a69a4966bf0de3be27ee
Luca Huettner <luca.huettner@gmail.com>
2018-03-21 21:08:15 +0100

    feat: updated list content to use api content

--- src/Locations.js
+++ src/Locations.js
@@ -7,11 +7,11 @@ class Locations extends Component {
       this.props.locations.map((location) => (
         <li key={location.id}>
           {location.name}
-          <img className='img-responsive' src='https://igx.4sqi.net/img/general/500x300/5g-RfDxQ_ktEWdD-I213xGBW45IbgWrrYK03GtDjuFs.jpg'/>
-          <address>
-            <strong>Address:</strong><br />
-            Auf dem Hohentwiel 2a
-          </address>
+          <img className='img-responsive' src={location.img}/>
+          <p>
+            <strong>Rating: </strong>
+            {location.rating}
+          </p>
         </li>
       ))
     );
