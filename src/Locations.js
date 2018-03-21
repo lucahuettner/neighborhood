e650cb1a23d8f4f40fb95ef93185f09b01610a68
Luca Huettner <luca.huettner@gmail.com>
2018-03-21 16:29:04 +0100

    feat: added image and address to list items

--- src/Locations.js
+++ src/Locations.js
@@ -5,7 +5,14 @@ class Locations extends Component {
   render() {
     return (
       this.props.locations.map((location) => (
-        <li key={location.name}>{location.name}</li>
+        <li key={location.name}>
+          {location.name}
+          <img className='img-responsive' src='https://igx.4sqi.net/img/general/500x300/5g-RfDxQ_ktEWdD-I213xGBW45IbgWrrYK03GtDjuFs.jpg'/>
+          <address>
+            <strong>Address:</strong><br />
+            Auf dem Hohentwiel 2a
+          </address>
+        </li>
       ))
     );
   }
