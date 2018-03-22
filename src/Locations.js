5ecd9480bb1bb7401ce2b0a08d49cc3aeac2b7d8
Luca Huettner <luca.huettner@gmail.com>
2018-03-22 12:43:55 +0100

    feat: when list item is clicked open specific location info

--- src/Locations.js
+++ src/Locations.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import LocationInfo from './LocationInfo.js';
+import {Route, Link} from 'react-router-dom';
+
 
 class Locations extends Component {
   render() {
     return (
       this.props.locations.map((location) => (
         <li key={location.id}>
-          <h4>{location.name}</h4>
-          <LocationInfo location={location}/>
+          <Link to={`/list/${location.id}`}>
+            <h4>{location.name}</h4>
+          </Link>
+          <Route path={`/list/${location.id}`} render={() => (
+            <LocationInfo location={location}/>
+            )}>
+          </Route>
         </li>
       ))
     );
