f8c4aee92644970e1329178d99d3e3d5a94af8c5
Luca Huettner <luca.huettner@gmail.com>
2018-03-27 14:20:38 +0200

    feat: added category selection

--- src/List.js
+++ src/List.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom';
 import Locations from './Locations.js';
 
 class List extends Component {
+  state = {
+    selected: 'all'
+  };
   render() {
     return (
       <div className='col-sm-3'>
+        <select defaultValue='all' onChange={(e) => this.setState({selected: e.target.value})}>
+          <option value='all'>All</option>
+          <option value='restaurants'>Restaurants</option>
+          <option value='leisure'>Leisure activities</option>
+          <option value='cultural'>Cultural places</option>
+        </select>
         <ul className='list-unstyled text-left'>
           <Locations
-            locations={this.props.locations}
+            locations={this.props.locations.filter((location) => location.category === this.state.selected)}
             animateMarker={this.props.animateMarker}/>
         </ul>
       </div>
